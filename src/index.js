95050a4979ae67e7fff9d8fa6c9b476a6c7b8a45
Keith Cullinan <kfcullinan@gmail.com>
2022-09-17 13:03:45 -0500

    added in imports to index.js and react installed

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
